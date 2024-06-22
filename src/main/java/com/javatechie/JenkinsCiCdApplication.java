b42b02b73473d24b13f1f091c4ba7b528b776631
priyeranj <priyeranjan.moscow@gmail.com>
2024-06-23 04:56:44 +0530

    fail build to get emmail

--- src/main/java/com/javatechie/JenkinsCiCdApplication.java
+++ src/main/java/com/javatechie/JenkinsCiCdApplication.java
@@ -13,6 +13,7 @@ public class JenkinsCiCdApplication {
 
     @GetMapping("/greetings/{name}")
     public String greetings(@PathVariable String name) {
+        name=name.split(" ")[0];
         return "Hello " + name + " Congratulations you have successfully completed Jenkins CI/CD demo !";
     }
 
